70ad1668f7d90fd958a0654d8a2c995890c25fbc
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-08-17 18:34:49 +0200

    added @CrossOrigin in SumsController

--- src/main/java/com/example/demo/controllers/SumsController.java
+++ src/main/java/com/example/demo/controllers/SumsController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RequestMapping("/sums")
 public class SumsController {
     @Autowired
